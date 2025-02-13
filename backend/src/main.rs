3583ce863c4828b96652596f957f17d4fa018d09
Harrison Leath <hleath@me.com>
2025-02-13 15:33:31 -0600

    Added POC for Axum with default route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,2 +1,30 @@
+use std::net::SocketAddr;
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    dotenv().ok();
+
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    tracing::info!("listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello world! What's good."
+}
+
