88a8721bc72453fbf6086b9dc29e006015bae1b8
Harrison Leath <hleath@me.com>
2025-02-16 01:28:39 -0600

    Updated brand color and removed dark mode

--- frontend/src/lib/components/app-header.svelte
+++ frontend/src/lib/components/app-header.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import * as Avatar from '$lib/components/ui/avatar/index.js';
-	import DarkModeSwitch from './dark-mode-switch.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { Button } from '$lib/components/ui/button';
 	import { useSidebar } from '$lib/components/ui/sidebar/context.svelte.js';
@@ -17,5 +15,4 @@
 		{/if}
 		<span class="text-lg font-semibold">EchelonGPT</span>
 	</div>
-	<DarkModeSwitch />
 </div>
