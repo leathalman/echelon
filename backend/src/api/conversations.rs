c75279abe05323155ac980ca1987c5b0ab01d512
Harrison Leath <hleath@me.com>
2025-02-17 14:16:22 -0600

    Addd POST for messages in a chat

--- backend/src/api/conversations.rs
+++ backend/src/api/conversations.rs
@@ -6,8 +6,9 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::Json;
 use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::sync::Arc;
-use tracing::info;
 
 /// GET /api/conversation/
 /// Authorized Endpoint -> JWT Required
@@ -65,10 +66,8 @@ pub async fn conversation_list_messages(
         .fetch_all(&state.db)
         .await;
 
-    info!("convo id is: {}", conversation_id);
-
     if query_result.is_err() {
-        let error_response = serde_json::json!({
+        let error_response = json!({
             "message": "Something bad happened while fetching messages..."
         });
         return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)));
@@ -76,11 +75,74 @@ pub async fn conversation_list_messages(
 
     let messages = query_result.unwrap();
 
-    let json_response = serde_json::json!({
+    let json_response = json!({
         "messages": messages
     });
 
     Ok(Json(json_response))
 
     // TODO: Only return success if UserID matches ownerID from Postgres, aka needs security
+}
+
+/// POST /api/conversation/{conversation_id}/messages
+#[axum::debug_handler]
+pub async fn conversation_new_message_handler(
+    Path(conversation_id): Path<i32>,
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<CreateMessageSchema>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    tracing::info!("Convo ID {}", conversation_id);
+
+    let query_result = sqlx::query_as!(
+        Message,
+        r#"
+                WITH new_message AS (
+                INSERT INTO chat.messages (conversation_id, content, role)
+                VALUES ($1, $2, $3)
+                RETURNING id, conversation_id, content, role as "role!: MessageRole", created_at
+            ),
+            update_conversation AS (
+                UPDATE chat.conversations
+                SET last_message_at = CURRENT_TIMESTAMP
+                WHERE id = $1
+            )
+            SELECT * FROM new_message
+            "#,
+        conversation_id,
+        payload.content,
+        payload.role as MessageRole
+    )
+        .fetch_one(&state.db)
+        .await;
+
+    match query_result {
+        Ok(message) => {
+            let message_response = json!({
+                "message_id": message.id
+            });
+
+            Ok((StatusCode::CREATED, Json(message_response)))
+        }
+        Err(e) => {
+            if e.to_string()
+                .contains("duplicate key value violates unique constraint")
+            {
+                let error_response = json!({
+                    "message": "Message with that title already exists",
+                });
+                return Err((StatusCode::CONFLICT, Json(error_response)));
+            }
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"message": format!("{:?}", e)})),
+            ))
+        }
+    }
+}
+
+// put whatever needs to be in the JSON body in here
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateMessageSchema {
+    pub content: String,
+    pub role: MessageRole,
 }
--- backend/src/api/router.rs
+++ backend/src/api/router.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::{routing::get, Router};
 use sqlx::{Pool, Postgres};
 
-use crate::api::conversations::{conversation_list_handler, conversation_list_messages};
+use crate::api::conversations::{conversation_list_handler, conversation_list_messages, conversation_new_message_handler};
 use crate::api::health::health_checker_handler;
 
 pub struct AppState {
@@ -16,7 +16,8 @@ pub fn create_router(app_state: Arc<AppState>) -> Router {
         .route("/api/conversations", get(conversation_list_handler))
         .route(
             "/api/conversations/:conversation_id/messages",
-            get(conversation_list_messages),
+            get(conversation_list_messages)
+                .post(conversation_new_message_handler),
         )
         .with_state(app_state)
 }
