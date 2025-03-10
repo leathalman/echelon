f7a5267c751786b2af254de429a0e0423789210c
Harrison Leath <hleath@me.com>
2025-03-09 23:50:06 -0500

    Modified avatar fallback on settings menu

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -135,7 +135,8 @@
 							<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 								<Avatar.Root class="h-8 w-8 rounded-lg">
 									<!--									<Avatar.Image src={user.avatar} alt={user.name} />-->
-									<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+									<Avatar.Fallback
+										class="rounded-lg">{data.user.first_name.at(0)}{data.user.last_name.at(0)}</Avatar.Fallback>
 								</Avatar.Root>
 								<div class="grid flex-1 text-left text-sm leading-tight">
 									<span class="truncate font-semibold">{data.user.first_name} {data.user.last_name}</span>
